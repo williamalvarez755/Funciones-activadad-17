95fa3f4b6a4a83e4ef4a5c04d78bc6f79cf8bd90
williamalvarez755 <williamalvarez1101@gmail.com>
2026-03-12 19:05:15 -0600

    creo el repostior y subo el primer ejecicio

---
+++ ejercicio 1/ejercicio 1/Program.cs
@@ -0,0 +1,11 @@
+﻿int funcionprincipal(int numero1, int numero2)
+{
+    return numero1 + numero2;
+}
+
+Console.WriteLine("Ingrese numero 1");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese numero 2");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(funcionprincipal(numero1, numero2));
